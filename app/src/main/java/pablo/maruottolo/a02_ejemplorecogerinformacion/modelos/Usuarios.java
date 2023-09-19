5088f337cb5ccbda460f1d0cbe75b18987533de8
pablomaruottolo <pablogabriel.maruottolo@cieep.com>
2023-09-19 12:46:00 +0200

    Initial commit

---
+++ app/src/main/java/pablo/maruottolo/a02_ejemplorecogerinformacion/modelos/Usuarios.java
@@ -0,0 +1,39 @@
+package pablo.maruottolo.a02_ejemplorecogerinformacion.modelos;
+
+public class Usuarios {
+    //atributos
+    private String email;
+    private String password;
+
+    //constructores
+
+    public Usuarios() {
+    }
+    public Usuarios(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    //getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios : " + "email='" + email + '\n' + ", password='" + password ;
+    }
+}
